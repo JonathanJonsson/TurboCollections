01dee6f696e8775a8092f1aed7a251fe46cde426
JonathanJonsson <jonathan.jonsson@student.forsbergsskola.se>
2022-01-19 11:29:31 +0100

    Started implementing Queue w poor dequeue implemntation

---
+++ TurboCollections.Tests/TurboQueueTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace TurboCollections.Tests;
+
+public class TurboQueueTests
+{
+	[Test]
+	public void EnqueueToQueueTest()
+	{
+		var queue = new TurboQueue<string>();
+		
+		queue.Enqueue("Hello");
+		Assert.AreEqual(1, queue.Count);
+	}
+	
+	
+	[Test]
+	public void PeekOnFirstElement()
+	{
+		var queue = new TurboQueue<string>();
+		
+		queue.Enqueue("Hi");
+		queue.Enqueue("Marc");
+		
+		Assert.AreEqual("Hi", queue.Peek()); //Check that peek looks at index 0
+		Assert.AreNotEqual("Marc", queue.Peek()); // checks that peek does not refer to last index for some reason.
+	}
+
+	[Test]
+	public void CheckThatEmptySetsToDefault()
+	{
+		var queue = new TurboQueue<string>();
+		
+		
+		queue.Enqueue("Hello");
+		queue.Clear();	
+		Assert.AreEqual(default, queue.Peek());
+			
+		
+	}
+	
+	
+	
+	
+}
--- TurboCollections/TurboQueue.cs
+++ TurboCollections/TurboQueue.cs
@@ -1,22 +1,70 @@
-﻿namespace TurboCollections;
+﻿using System.Diagnostics.Metrics;
+
+namespace TurboCollections;
 
 public class TurboQueue<T>
 {
 	
 	// returns the current amount of items contained in the stack.
 	public int Count { get; private set; }
+	public int QueueSize { get; private set; }
+	private T[] queue = Array.Empty<T>();
 	
 	// // adds one item to the back of the queue.
-	// void Enqueue(T item);
+	public void Enqueue(T item)
+	{
+		EnsureQueueSize(Count+1);
+		queue[Count++] = item;
+	}
 	
 	// // returns the item in the front of the queue without removing it.
-	// T Peek();
+	public T Peek()
+	{
+		return queue[0];
+	}
 	
+	//POOR IMPLEMENTATION
 	// // returns the item in the front of the queue and removes it at the same time.
-	// T Dequeue();
+	public T Dequeue()
+	{
+		
+		
+		
+		
+		return default;
+	}
 	
 	// // removes all items from the queue.
-	// void Clear();
-	
-	
+	public void Clear()
+	{
+		for (int i = 0; i < Count; i++)
+		{
+			queue[i] = default;
+		}
+
+		Count = 0;
+	}
+
+
+
+	private void EnsureQueueSize(int sizeRequired)
+	{
+		if (queue.Length >= sizeRequired)
+		{
+			return;
+		}
+
+		int newSize = Math.Max(sizeRequired, queue.Length*2);
+
+		T[] tempQueue = new T[newSize];
+
+		for (int i = 0; i < Count; i++)
+		{
+			tempQueue[i] = queue[i];
+		}
+
+		queue = tempQueue;
+
+	}	
 }
+

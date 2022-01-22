99fa481005a57220f81d4c595d1221fbc4af085d
JonathanJonsson <jonathan.jonsson@student.forsbergsskola.se>
2022-01-22 22:26:32 +0100

    Add turbolinkedstack.tests LatestEntryIsReturnedAndRemovedWhenPop

--- TurboCollections.Tests/TurboLinkedStack.Tests.cs
+++ TurboCollections.Tests/TurboLinkedStack.Tests.cs
@@ -33,8 +33,25 @@ public class TurboLinkedStack_Tests
 		linkedStack.Push("World");
 		linkedStack.Clear();
 		Assert.Zero(linkedStack.Count);
-		Assert.Zero(linkedStack.Count);
 	}
+
+	[Test]
+	public void LatestEntryIsReturnedAndRemovedWhenPop()
+	{
+		var linkedStack = new TurboLinkedStack<int>();
+		
+		linkedStack.Push(8);
+		linkedStack.Push(5);
+
+		var g = linkedStack.Pop();
+		Assert.AreEqual(5, g);
+		Assert.AreEqual(8, linkedStack.Peek());
+		Assert.AreEqual(1, linkedStack.Count);
+		 
+	}
+	
+	
+	
 	
 	
 	

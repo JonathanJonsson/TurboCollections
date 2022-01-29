590503ed7994eda2dc192dae676345f8b8ffa244
JonathanJonsson <jonathan.jonsson@student.forsbergsskola.se>
2022-01-29 12:32:06 +0100

    Implemented quick sort with some pseudocode help
    https://www.geeksforgeeks.org/quick-sort/

--- TurboCollections/TurboQuickSorting.cs
+++ TurboCollections/TurboQuickSorting.cs
@@ -1,29 +1,42 @@
-﻿using System.Security.AccessControl;
-using System.Xml.Xsl;
+﻿namespace TurboCollections;
 
-namespace TurboCollections;
-
+//implemented with some pseudocode help from: https://www.geeksforgeeks.org/quick-sort/
 public static class TurboQuickSorting
 {
 	public static void TurboQuickSort(TurboList<int> list, int left, int right)
 	{
-		
-		 
-		 
-		
+		if (left < right)
+		{
+			var pi = Partition(list, left, right);
+			TurboQuickSort(list, left, pi - 1);
+			TurboQuickSort(list, pi + 1, right);
+		}
 	}
 
-	public static int Partition(TurboList<int> list, int left, int right)
+	private static int Partition(TurboList<int> list, int left, int right)
 	{
-
 		
+		var pivot = list.Get(right);
+		var i = left - 1;
 
+		for (var j = left; j <= right; j++)
+		{
+			if (list.Get(j) < pivot)
+			{
+				i++;
+				SwapPositions(list, i, j);
+			}
+		}
+
+		SwapPositions(list, i + 1, right);
+
+		return i + 1;
 	}
-	
-	public static void SwapPositions(TurboList<int> list, int firstIndex, int secondIndex)
+
+	private static void SwapPositions(TurboList<int> list, int firstIndex, int secondIndex)
 	{
 		var temp = list.Get(firstIndex);
-		list.Set(firstIndex, list.Get(secondIndex)) ;
+		list.Set(firstIndex, list.Get(secondIndex));
 		list.Set(secondIndex, temp);
 		// Console.WriteLine($"Swaped: {temp} with {list.Get(firstIndex)}");
 	}

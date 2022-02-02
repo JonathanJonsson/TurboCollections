a1d557b0a85915c6faf239960ec5db0d82bfc4c5
JonathanJonsson <jonathan.jonsson@student.forsbergsskola.se>
2022-02-02 21:23:14 +0100

    Added TurboHashSets and tests + added skeleton of classes

---
+++ TurboCollections.Tests/TurboHashSetTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace TurboCollections.Tests;
+
+ 
+public class TurboHashSetTests
+{
+	[Test]
+	public void test()
+	{
+		
+	}
+}
---
+++ TurboCollections/TurboHashSet.cs
@@ -0,0 +1,26 @@
+﻿namespace TurboCollections;
+
+public class TurboHashSet<T>
+{
+	private int initialSize = 5;
+	private T[] hashSet = new T[5];
+
+	public bool Insert(T item)
+	{
+		return false;
+	}
+
+	public bool Exists(T item)
+	{
+		return false;
+	}
+
+	public bool Remove(T item)
+	{
+		return false;
+	}
+
+
+
+
+}

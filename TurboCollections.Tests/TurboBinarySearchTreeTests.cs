154851bbcae733bb778ec90c5c4daaab9ad99b06
JonathanJonsson <jonathan.jonsson@student.forsbergsskola.se>
2022-02-02 18:32:36 +0100

    Added test so that number sorts in correctly by size

--- TurboCollections.Tests/TurboBinarySearchTreeTests.cs
+++ TurboCollections.Tests/TurboBinarySearchTreeTests.cs
@@ -14,5 +14,17 @@ public class TurboBinarySearchTreeTests
 		Assert.IsNull(BST.root.left);
 		Assert.IsNull(BST.root.right);
 	}
+
+	[Test]
+	public void HigherNumbersGoesToTheRightWhileLowerToTheLeft()
+	{
+		var BST = new TurboBinarySearchTree();
+		BST.Insert(5);
+		BST.Insert(2);
+		BST.Insert(10);
+		
+		Assert.AreEqual(10, BST.root.right.value);
+		Assert.AreEqual(2, BST.root.left.value);
+	}
 	
 }
